64a32687733dd636244fc1cde3bc63819931262a
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-10 13:01:58 +0530

    ts for functions

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
 // Just for Debugging
 let age: number = 20;
-if(age < 50 )
-    age+=10
+if (age < 50)
+    age += 10
 console.log(age);
 
 // If we don't specify the type then ts itself declare it as any which is waste all the motive to use the ts.
 
 let num;
-console.log(typeof(num));
+console.log(typeof (num));
 
 // if type of variable is already defined then intelllisense show related properties to autocomplete. Yayyyy!
 
-let n=[1,2,3];
-n.map(item=>item.toExponential())
+let n = [1, 2, 3];
+n.map(item => item.toExponential())
+
+//  Ts for functions
+function incomeTax(income: number, taxYear: number): number {
+    if (taxYear > 20_000) {
+        return income * 20;
+    }
+    return income * 10;
+}
